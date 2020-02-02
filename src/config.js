7bd155469b979ed3c97abfb5d154bd973be63a73
Sean Maxwell <sa.maxwell@yahoo.com>
2020-02-02 10:59:59 -0600

    Setting up security

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "serverless-notes-app-uploads-sean"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "g8fnnqunpj.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_fMy0h85Og",
+        APP_CLIENT_ID: "7j1qbahkmfpv8pgouktfublss9",
+        IDENTITY_POOL_ID: "us-east-1:05411f88-b805-4142-a25d-7c665734df60"
+    }
+};
